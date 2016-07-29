0a28bd5b44e7a9765f997ca22ada5ba542cbba3b
kamarado <lkamarado@gmail.com>
2016-07-29 17:44:23 +0300

    First attempt to introduce russian translation.

---
+++ example/translations/qhexedit_ru.ts
@@ -0,0 +1,418 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>About HexEdit</source>
+        <translation type="obsolete">О HexEdit</translation>
+    </message>
+    <message>
+        <source>The HexEdit example is a short Demo of the QHexEdit Widget.</source>
+        <translation type="obsolete">HexEdit это простая демонстрация виджета QHexEdit.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="61"/>
+        <source>Save As</source>
+        <translation>Сохранить как...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="71"/>
+        <location filename="../mainwindow.cpp" line="93"/>
+        <source>Save To Readable File</source>
+        <translation>Сохранить как текстовый файл</translation>
+    </message>
+    <message>
+        <source>HexEdit</source>
+        <translation type="obsolete">HexEdit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="27"/>
+        <source>About QHexEdit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="28"/>
+        <source>The QHexEdit example is a short Demo of the QHexEdit Widget.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="76"/>
+        <location filename="../mainwindow.cpp" line="98"/>
+        <location filename="../mainwindow.cpp" line="343"/>
+        <source>QHexEdit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="77"/>
+        <location filename="../mainwindow.cpp" line="99"/>
+        <location filename="../mainwindow.cpp" line="344"/>
+        <source>Cannot write file %1:
+%2.</source>
+        <translation>Невозможно записать файл %1:
+%2.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="87"/>
+        <location filename="../mainwindow.cpp" line="109"/>
+        <location filename="../mainwindow.cpp" line="355"/>
+        <source>File saved</source>
+        <translation>Файл сохранен</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="121"/>
+        <source>Overwrite</source>
+        <translation>Перезаписать</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="123"/>
+        <source>Insert</source>
+        <translation>Вставить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="168"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Открыть...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="170"/>
+        <source>Open an existing file</source>
+        <translation>Открыть существующий файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="173"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="175"/>
+        <source>Save the document to disk</source>
+        <translation>Сохранить документ на диск</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="178"/>
+        <source>Save &amp;As...</source>
+        <translation>Сохранить &amp;как...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="180"/>
+        <source>Save the document under a new name</source>
+        <translation>Сохранить документ с новым именем</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="183"/>
+        <source>Save &amp;Readable...</source>
+        <translation>Сохранить Č&amp;как текст...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="184"/>
+        <source>Save document in readable form</source>
+        <translation>Сохранить документ в текстовом виде</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="187"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Выход</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Exit the application</source>
+        <translation>Выйти из приложения</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="192"/>
+        <source>&amp;Undo</source>
+        <translation>&amp;Отменить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="196"/>
+        <source>&amp;Redo</source>
+        <translation>&amp;Повторить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="200"/>
+        <source>&amp;Save Selection Readable...</source>
+        <translation>&amp;Сохранить выделение как текст...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="201"/>
+        <source>Save selection in readable form</source>
+        <translation>Сохранить выделение в текстовом виде</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>&amp;About</source>
+        <translation>&amp;О программе</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="205"/>
+        <source>Show the application&apos;s About box</source>
+        <translation>Показать окно "О программе"</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="208"/>
+        <source>About &amp;Qt</source>
+        <translation>O &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="209"/>
+        <source>Show the Qt library&apos;s About box</source>
+        <translation>Показать информацию о библиотеке Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="219"/>
+        <source>Find next occurrence of the searched pattern</source>
+        <translation>Найти следующее вхождение шаблона</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="222"/>
+        <source>&amp;Options</source>
+        <translation>&amp;Настройки...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="223"/>
+        <source>Show the Dialog to select applications options</source>
+        <translation>Показать окно с настройками приложения</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="212"/>
+        <source>&amp;Find/Replace</source>
+        <translation>Найти/Заменить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="214"/>
+        <source>Show the Dialog for finding and replacing</source>
+        <translation>Показать диалог поиска и замены</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="217"/>
+        <source>Find &amp;next</source>
+        <translation>Найти далее</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="229"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="237"/>
+        <source>&amp;Edit</source>
+        <translation>П&amp;равка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Справка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="256"/>
+        <source>Address:</source>
+        <translation>Адрес:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="267"/>
+        <source>Size:</source>
+        <translation>Размер:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="278"/>
+        <source>Mode:</source>
+        <translation>Режим:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="287"/>
+        <source>Ready</source>
+        <translation>Готов</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="292"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="302"/>
+        <source>SDI</source>
+        <translation>SDI</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="303"/>
+        <source>Cannot read file %1:
+%2.</source>
+        <translation>Невозможно прочесть файл %1:
+%2.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="314"/>
+        <source>File loaded</source>
+        <translation>Файл загружен</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <source>Dialog</source>
+        <translation type="obsolete">Диалог</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="14"/>
+        <source>QHexEdit - Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="20"/>
+        <source>Flags</source>
+        <translation>Флаги</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="26"/>
+        <source>ReadOnly</source>
+        <translation>ТолькоЧтение</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="33"/>
+        <source>Higlighting</source>
+        <translation>Подсветка</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="40"/>
+        <source>Overwrite Mode</source>
+        <translation>Режим перезаписи</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="47"/>
+        <location filename="../optionsdialog.ui" line="195"/>
+        <source>Address Area</source>
+        <translation>Адресная область</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="54"/>
+        <source>Ascii Area</source>
+        <translation>Область ASCII</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="64"/>
+        <source>Colors and Fonts</source>
+        <translation>Цвета и шрифты</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="70"/>
+        <source>Highlighting Color</source>
+        <translation>Цвет подсветки</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="102"/>
+        <source>Address Area Color</source>
+        <translation>Цвет адресной области</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="159"/>
+        <source>Selection Color</source>
+        <translation>Цвет выделения</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="166"/>
+        <source>Widget Font</source>
+        <translation>Шрифт виджета</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="185"/>
+        <source>01 23 45 67 89 ab cd ef</source>
+        <translation>01 23 45 67 89 ab cd ef</translation>
+    </message>
+    <message>
+        <location filename="../optionsdialog.ui" line="201"/>
+        <source>Address Area Width</source>
+        <translation>Ширина адресной области</translation>
+    </message>
+</context>
+<context>
+    <name>SearchDialog</name>
+    <message>
+        <source>Dialog</source>
+        <translation type="obsolete">Диалог</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="14"/>
+        <source>QHexEdit - Find/Replace</source>
+        <translation>Поиск/замена</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="22"/>
+        <source>Find</source>
+        <translation>Поиск</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="29"/>
+        <location filename="../searchdialog.ui" line="65"/>
+        <source>Hex</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="34"/>
+        <location filename="../searchdialog.ui" line="70"/>
+        <source>UTF-8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="58"/>
+        <source>Replace</source>
+        <translation>Замена</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="94"/>
+        <source>Options</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="100"/>
+        <source>&amp;Backwards</source>
+        <translation>Назад</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="107"/>
+        <source>&amp;Prompt on replace</source>
+        <translation>Спросить перед заменой</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="121"/>
+        <source>&amp;Find</source>
+        <translation>Найти</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="124"/>
+        <source>F3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="134"/>
+        <source>&amp;Replace</source>
+        <translation>Заменить</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="141"/>
+        <source>Replace &amp;All</source>
+        <translation>Заменить Всё</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.ui" line="148"/>
+        <source>&amp;Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.cpp" line="71"/>
+        <location filename="../searchdialog.cpp" line="97"/>
+        <source>QHexEdit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.cpp" line="71"/>
+        <source>%1 occurrences replaced.</source>
+        <translation>%1 вхождений заменено</translation>
+    </message>
+    <message>
+        <location filename="../searchdialog.cpp" line="98"/>
+        <source>Replace occurrence?</source>
+        <translation>Заменить?</translation>
+    </message>
+</context>
+</TS>
