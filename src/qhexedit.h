7456ec5a188deb62e5a3de29f11062320cfd758f
Pavel Vojtyuk <voytuk.pavel@nicetu.spb.ru>
2016-08-12 12:02:54 +0300

    Bytes number per line became a property.

--- src/qhexedit.h
+++ src/qhexedit.h
@@ -123,6 +123,9 @@ class QHexEdit : public QAbstractScrollArea
     /*! Set the font of the widget. Please use fixed width fonts like Mono or Courier.*/
     Q_PROPERTY(QFont font READ font WRITE setFont)
 
+    /*! Set and get bytes number per line.*/
+    Q_PROPERTY(int bytesPerLine READ bytesPerLine WRITE setBytesPerLine)
+
 public:
     /*! Creates an instance of QHexEdit.
     \param parent Parent widget of QHexEdit.
@@ -142,8 +145,8 @@ public:
     */
     QByteArray dataAt(qint64 pos, qint64 count=-1);
 
-    /*! Givs back the data into a \param iODevice starting at position \param pos 
-    and delivering \param count bytes. 
+    /*! Givs back the data into a \param iODevice starting at position \param pos
+    and delivering \param count bytes.
     */
     bool write(QIODevice &iODevice, qint64 pos=0, qint64 count=-1);
 
@@ -300,6 +303,18 @@ public:
     QColor selectionColor();
     void setSelectionColor(const QColor &color);
 
+    int bytesPerLine();
+    void setBytesPerLine(int count);
+
+    bool lineNumberArea() const;
+    void setLineNumberArea(bool lineNumberArea);
+
+    QColor lineNumberAreaColor() const;
+    void setLineNumberAreaColor(const QColor &color);
+
+    int lineNumberWidth() const;
+    void setLineNumberWidth(int width);
+
 protected:
     // Handle events
     void keyPressEvent(QKeyEvent *event);
@@ -362,6 +377,8 @@ private:
     QBrush _brushHighlighted;
     QPen _penHighlighted;
     bool _readOnly;
+    int _bytesPerLine;
+    int _hexCharsInLine;
 
     // other variables
     int _addrDigits;                            // real no of addressdigits, may be > addressWidth
