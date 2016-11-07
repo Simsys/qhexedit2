4d36ef3ebfc018bc6faac8cc2411c079d3ada341
Winfried Simon <winfried.simon@gmail.com>
2016-11-07 20:45:43 +0100

    v0.8.0

--- doc/html/search/functions_8.js
+++ doc/html/search/functions_8.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['selectiontoreadablestring',['selectionToReadableString',['../class_q_hex_edit.html#a2ada1230ace92da4e1f006b88ab3b503',1,'QHexEdit']]],
   ['setdata',['setData',['../class_q_hex_edit.html#ab5221718179da17cbcd9db3bf6574df7',1,'QHexEdit']]],
-  ['setfont',['setFont',['../class_q_hex_edit.html#adc7c0a08db5ae15fc842d7b4fd8edad6',1,'QHexEdit']]]
+  ['setfont',['setFont',['../class_q_hex_edit.html#a57221461a9c9e1b2c0e1882788ac9911',1,'QHexEdit']]]
 ];
