7becaba010e6ddfa1ca43a923a7ebda1b5670841
Winfried Simon <winfried.simon@gmail.com>
2016-11-08 21:01:58 +0100

    v0.8.1

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ dirname = os.path.dirname(__file__)
 
 setup(
     name='QHexEdit',
-    version='0.7.2',
+    version='0.8.1',
     ext_modules=[
         Extension(
             "qhexedit",
--- src/qhexedit.h
+++ src/qhexedit.h
@@ -11,7 +11,7 @@
 /** \mainpage
 QHexEdit is a binary editor widget for Qt.
 
-\version Version 0.7.8
+\version Version 0.8.1
 \image html qhexedit.png
 */
 
