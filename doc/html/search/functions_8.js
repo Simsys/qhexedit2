68c18a25a6e012189fdf5c24d69ef1fab9d0270a
Simsys <winfried.simon@gmail.com>
2019-03-16 17:11:08 +0100

    v0.8.6

--- doc/html/search/functions_8.js
+++ doc/html/search/functions_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['selecteddata',['selectedData',['../class_q_hex_edit.html#ac1f9ac4d3ec5bedabd3aeedda3191b20',1,'QHexEdit']]],
   ['selectiontoreadablestring',['selectionToReadableString',['../class_q_hex_edit.html#a2ada1230ace92da4e1f006b88ab3b503',1,'QHexEdit']]],
   ['setdata',['setData',['../class_q_hex_edit.html#ab5221718179da17cbcd9db3bf6574df7',1,'QHexEdit']]],
   ['setfont',['setFont',['../class_q_hex_edit.html#a57221461a9c9e1b2c0e1882788ac9911',1,'QHexEdit']]]
