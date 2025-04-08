19aabe4fb1df2744156e06e3736ab4808de365df
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2025-04-08 08:56:41 +0300

    Added copyright and licence
    Adding a licence to the file is a requirement of the Debian maintainers.

--- src/chunks.cpp
+++ src/chunks.cpp
@@ -1,3 +1,18 @@
+/*
+* Copyright (C) 2015-2016 Winfried Simon
+*
+* This software may be used under the terms of the GNU Lesser General 
+* Public License version 2.1 as published by the Free Software Foundation 
+* and appearing in the file license.txt included in the packaging of this file.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "chunks.h"
 #include <limits.h>
 
